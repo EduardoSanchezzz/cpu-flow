11376e59503bb2eda8506373c9a954975d7dd506
EduardoSanchezzz <esanch747@gmail.com>
2024-09-26 15:21:57 -0400

    fix clock

--- src/nodes/Clock.tsx
+++ src/nodes/Clock.tsx
@@ -14,17 +14,15 @@ function Clock({ id, data }: NodeProps<ClockNode>) {
     <div
       className='clock'
     >
-      <div>
-        <div>clk: {!!data.clk ? 'HIGH': 'LOW'}</div>
-        <div>step: {data.step < 0 ? 'X': data.step}</div>
-      </div>
+      CLOCK
       <button
       // the exoression for the clock step is a lil weird but it just increments the step only on positive clock edge
         onClick={() => updateNodeData(id, { clk: !!data.clk ? 0 : 1, step: Math.floor(data.step + 1 - 0.5*data.clk) })}
         value={data.clk}
-        className='step'
+        className={`step${!data.clk ? ' step-low' : ''}`}
       >
         Step
+         {/* {data.step < 0 ? 'x' : data.step} */}
       </button>
       <button className='reset' onClick={reset}>reset</button>
       <Handle
